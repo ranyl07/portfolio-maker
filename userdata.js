8e75adae98c47da1d09bff469c0af6bc4db39916
ranyl07 <r.ladhem.inf@lagh-univ.dz>
2026-04-22 20:55:37 +0100

    change

---
+++ userdata.js
@@ -0,0 +1,7 @@
+let q = document.querySelectorAll(".fill"); //we bring all question divs as an array//
+function appear() {
+    for (let i=0 ; i< q.length;i++) {
+        q[i].Style.display = "content";
+    }
+    
+}
