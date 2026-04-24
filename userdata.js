b7c06ff878df3c70f110ca0b26de9fbe7d3edf57
ranyl07 <r.ladhem.inf@lagh-univ.dz>
2026-04-24 05:13:44 +0100

    name changed

--- userdata.js
+++ userdata.js
@@ -5,6 +5,8 @@ let w = document.getElementById("welcome");
  let start = document.querySelector(".Start");
  let make = document.querySelector("#make");
  let pre = document.querySelectorAll(".pre");
+ let y = document.querySelectorAll(".btn");
+ let n = document.querySelectorAll(".no");
  //we bring the next button//
  let c=0;
  start.addEventListener("click", function(){
@@ -12,7 +14,7 @@ let w = document.getElementById("welcome");
     make.style.display = "none";
     w.style.display = "block";
  q[0].style.display = "block"; });
- next.forEach(function(button ,i) {
+ next.forEach(function(button) {
     button.addEventListener("click", function(){
         if (c < q.length - 1){
         q[c].style.display = "none";
@@ -20,7 +22,7 @@ let w = document.getElementById("welcome");
         q[c].style.display = "block";}
     });
     });
-pre.forEach(function(button, i) {
+pre.forEach(function(button) {
     button.addEventListener("click", function(){
         if (c > 0){
             q[c].style.display = "none";
@@ -28,4 +30,25 @@ pre.forEach(function(button, i) {
             q[c].style.display = "block";
         }
     });
+});
+y.forEach(function(b) {
+    b.addEventListener("click", function(){
+        q[c].style.display = "none";
+        c++;
+        q[c].style.display = "block";
+    });
+});
+n.forEach(function(b) {
+    b.addEventListener("click", function(){
+        q[c].style.display = "none";
+        c = c + 2;
+        q[c].style.display = "block";
+    });
+});
+let add = document.querySelector(".add");
+add.addEventListener("click", function(){
+    let input = document.querySelector("input").v;
+    input.type = "text";
+    input.placeholder = "Enter a skill";
+    q[c].appendChild(input);
 });
