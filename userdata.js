fcc801acbe1cc8129d5dafd98e9f2c91c09d7a64
ranyl07 <r.ladhem.inf@lagh-univ.dz>
2026-04-24 02:10:59 +0100

    java script

--- userdata.js
+++ userdata.js
@@ -1,8 +1,31 @@
-let q = document.querySelectorAll(".fill"); //we bring all question divs as an array//
-let d = document.getElementsByTagName("h1");
-function appear() {
-    for (let i=0 ; i< q.length;i++) {
-        q[i].Style.display = "content";
-    }
-    
-}
+let q = document.querySelectorAll(".fill");
+let w = document.getElementById("welcome");
+ //we bring all question divs as an array//
+ let next = document.querySelectorAll(".next");
+ let start = document.querySelector(".Start");
+ let make = document.querySelector("#make");
+ let pre = document.querySelectorAll(".pre");
+ //we bring the next button//
+ let c=0;
+ start.addEventListener("click", function(){
+    start.style.display = "none";
+    make.style.display = "none";
+    w.style.display = "block";
+ q[0].style.display = "block"; });
+ next.forEach(function(button ,i) {
+    button.addEventListener("click", function(){
+        if (c < q.length - 1){
+        q[c].style.display = "none";
+        c++;
+        q[c].style.display = "block";}
+    });
+    });
+pre.forEach(function(button, i) {
+    button.addEventListener("click", function(){
+        if (c > 0){
+            q[c].style.display = "none";
+            c--;
+            q[c].style.display = "block";
+        }
+    });
+});
