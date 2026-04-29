13d903461662faa8610c03d2f53b363482add213
ranyl07 <r.ladhem.inf@lagh-univ.dz>
2026-04-29 15:48:08 +0100

    Restore latest work

---
+++ change.js
@@ -0,0 +1,15 @@
+const user ={
+  name : "",
+  email: "",
+  phone : "",
+  tagline: "",
+  About : "",
+};
+let n = document.querySelector("h1");
+let e = document.querySelector("link");
+let p = document.querySelector("#phone");
+let t = document.querySelector("em");
+let a = document.querySelector("#about");
+function submit(){
+ n =
+}
